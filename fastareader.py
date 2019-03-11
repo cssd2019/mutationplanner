d7f63618028dd94f7a459fc5c042fdef00e6a1a1
Illimar Hugo Rekand <Illimar.Rekand@uib.no>
2019-03-11 20:40:12 +0100

    converts fasta file to a dictionary with IDs as keys and sequence as values

---
+++ fastareader.py
@@ -0,0 +1,23 @@
+from Bio import SeqIO, Seq
+
+#seq_dict = SeqIO.to_dict(SeqIO.parse("test.fasta", "fasta"))
+#print(seq_dict)
+#print("######################")
+
+def fasta_to_dic(filename):
+	seq_dict = {}
+	for sequence in SeqIO.parse(filename, "fasta"):#
+		#test.fasta: 
+		#print(sequence.id)
+		s = sequence.seq
+		#print(repr(s))
+		s = str(sequence.seq)
+		#obtaining a sequence string from a .seq object is done with str()
+		#print(s)
+		#print(len(sequence))
+		seq_dict[sequence.id] = s
+
+	return seq_dict
+
+a = fasta_to_dic("test.fasta")
+print(a)
